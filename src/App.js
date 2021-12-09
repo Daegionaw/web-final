dd91cbdce3ef3ca508e9240c954337f1d0a7ae83
Daegiona Wilson <Daegiona.Wilson@sckans.edu>
2021-12-09 10:45:57 -0600

    edit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
-import Hero from './Hero';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+import logo from './brand.png'; 
+import AlissaEd from './AlissaEd.jpg';
+import KobiEd from './KobiEd.jpg';
+import AvaEd from './AvaEd.jpg';
+
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Hero />
+        <BrowserRouter>
+            <div className="header">
+                <img src={ logo } className="brand" />
+                 <div className="social-media">
+                    Connect on 
+                        <a href="https://facebook.com">
+                             ⓕ
+                        </a>
+                </div>
+            </div>
+            <div className="nav">
+             <a className="Home" href= "./Hero">Home</a>
+             <a className="About" href= "./Hero">About</a>
+             <a className="Contact" href= "./Hero">Contact</a>
+             <a className="Pricing" href= "./Hero">Pricing</a>
+             <a className="Reviews" href= "./Hero">Reviews</a>
+            </div>
+            <div className="navv">
+                </div>
+            <div className="photos">
+                <img src={ AlissaEd } className= "Pic" />
+                <img src={ KobiEd } className= "Pic2" />
+                <img src={ AvaEd } className= "Pic3" />
+            </div>
+            <div className="almost">
+            <img src={ logo } className="brand2" />
+            </div>
+            <div className="footer">
+            Located in Cowley County, KS
+            Portrait and Sports Photographer
+            </div>
+            <div className= "bottom">
+            "Whether you're shy or outgoing, Dae will make you feel loved"
+            </div>
+        
+            <Routes>
+                <Route path="/test" element={
+                    <div>
+                        Test
+                    </div>
+                } />
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
